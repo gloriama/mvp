66b8d6b11ef535723f92b782da9ccba34213f0eb
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-04 12:31:40 -0800

    Fills out view and controller to allow storage of goal objects

--- client/app.js
+++ client/app.js
@@ -1,8 +1,27 @@
+var DEFAULT_GOAL_NAME = '';
+var DEFAULT_GOAL_FREQ = 1;
+var DEFAULT_GOAL_POINTS = 10;
+
 angular.module('app', [])
 .controller('goalCtrl', function($scope) {
+  //temp properties used in view to store info for the new goal
+  $scope.goalName = DEFAULT_GOAL_NAME;
+  $scope.goalFreq = DEFAULT_GOAL_FREQ;
+  $scope.goalPoints = DEFAULT_GOAL_POINTS;
+
+  //properties used more generally
   $scope.storage = [];
   $scope.add = function() {
-    $scope.storage.push($scope.goal);
-    $scope.goal = '';
-  }
+    var goal = {
+      name: $scope.goalName,
+      freq: $scope.goalFreq,
+      points: $scope.goalPoints
+    };
+    $scope.storage.push(goal);
+
+    //reset defaults for temp properties to appear in view
+    $scope.goalName = DEFAULT_GOAL_NAME;
+    $scope.goalFreq = DEFAULT_GOAL_FREQ;
+    $scope.goalPoints = DEFAULT_GOAL_POINTS;
+  };
 });
